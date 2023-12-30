3c36811437981445902333542b6dde11990226fc
Максимилиан Мамиляев <maksimilianmamilaev@Maksimilians-MacBook-Pro.local>
2023-12-30 23:25:24 +0300

    --

--- internal/handlers/notion/notion.go
+++ internal/handlers/notion/notion.go
@@ -22,6 +22,7 @@ type NotionTables struct {
 }
 
 func (n *NotionTables) UpdateDatabases() {
+	tables := []domain.NotionTable{}
 	coins := make(map[string]bool)
 	ids, err := n.notionProvider.GetDatabases()
 	if err != nil {
@@ -33,6 +34,7 @@ func (n *NotionTables) UpdateDatabases() {
 		if err != nil {
 			fmt.Println("Error getting database")
 		}
+		tables = append(tables, table)
 		for _, row := range table.Rows {
 			common.AddString(coins, row.Coin)
 		}
@@ -40,9 +42,49 @@ func (n *NotionTables) UpdateDatabases() {
 	}
 
 	uniqueCoins := common.GetUniqueValues(coins)
-	for _, coin := range uniqueCoins {
-		fmt.Print(n.binanceProvider.GetCoinPrice(coin))
+	coinsPrices := n.GetCoinsPrices(uniqueCoins)
+
+	for databaseID, database := range tables {
+		for rowID, row := range database.Rows {
+			updatedRow, err := UpdateCoinPrice(row, coinsPrices)
+			if err != nil {
+				fmt.Println("Error updating coin price")
+			}
+			updatedRow, err = CointGains(updatedRow)
+			if err != nil {
+				fmt.Println("Error updating gain")
+			}
+			tables[databaseID].Rows[rowID] = updatedRow
+
+		}
+
 	}
+	fmt.Print(tables)
+}
+
+func (n *NotionTables) GetCoinsPrices(coins []string) map[string]float64 {
+	coinsPrices := make(map[string]float64)
+	for _, coin := range coins {
+		price, err := n.binanceProvider.GetCoinPrice(coin)
+		if err != nil {
+			fmt.Println("Error getting coin price")
+		}
+		coinsPrices[coin] = price
+	}
+	return coinsPrices
+}
+
+// write verification on no coin price
+func UpdateCoinPrice(row domain.NotionTableRow, coins map[string]float64) (domain.NotionTableRow, error) {
+	row.CurrentCointPrice = float64(coins[row.Coin])
+	return row, nil
+}
+
+// write verification on row.SoldAmoint == 0
+func CointGains(row domain.NotionTableRow) (domain.NotionTableRow, error) {
+	row.PercentageGain = float64((row.CurrentCointPrice*row.CoinAmount - row.SoldAmount) / row.SoldAmount)
+	return row, nil
+
 }
 
 func New(notionProvider NotionAPI, binanceProvider BinanceAPI) *NotionTables {
