2232fec64ac47202f6fecfe4279e9ca688407728
Максимилиан Мамиляев <maksimilianmamilaev@Maksimilians-MacBook-Pro.local>
2023-12-31 14:12:13 +0300

    Added env

--- cmd/cryptoliveNotion/main.go
+++ cmd/cryptoliveNotion/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/IrishBoy/CryptoLive/internal/domain"
 	notionHandler "github.com/IrishBoy/CryptoLive/internal/handlers/notion"
 	"github.com/IrishBoy/CryptoLive/internal/providers/binance"
 	"github.com/IrishBoy/CryptoLive/internal/providers/notion"
+	"github.com/joho/godotenv"
 )
 
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func main() {
-	notionClient := domain.NewNotionClient("")
+	API_NOTION_KEY := goDotEnvVariable("NOTION_API_KEY")
+	notionClient := domain.NewNotionClient(API_NOTION_KEY)
 	notionInstance := &notion.Notion{
 		NotionClient: *notionClient,
 	}
